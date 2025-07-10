a97a90d3f63320669811074083bb27de90f20276
Joris <wassinkjoris@gmail.com>
2025-07-10 13:00:58 +0200

    Add Day 2 part 2

--- 2024/Day2/Day2Solution/Day2Solution/Program.cs
+++ 2024/Day2/Day2Solution/Day2Solution/Program.cs
@@ -6,13 +6,13 @@
     {
         static void Main(string[] args)
         {
-            Solution.Part1Solution("input.txt");
+            Solution.solution("input.txt");
         }
     }
 
     internal class Solution
     {
-        public static void Part1Solution(string path)
+        public static void solution(string path)
         {
             var safeAmount = 0;
             
@@ -23,12 +23,13 @@
                 {
                     var line = sr.ReadLine();
                     if (line == null) continue;
-                    
-                    var parts = line.Split();
-                    if (IsSafe(parts))
+
+                    var parts = line.Split().Select(s => int.Parse(s)).ToList();
+                    if (IsSafeWithDampener(parts))
                     {
                         safeAmount++;
                     }
+                    
                 }
                 Console.WriteLine(safeAmount);
                 
@@ -40,15 +41,15 @@
             }
         }
 
-        private static bool IsSafe(string[] parts)
+        private static bool IsSafe(List<int> parts)
         {
             var increasing = false;
 
             //counting
-            for (var i = 1; i < parts.Length; i++)
+            for (var i = 1; i < parts.Count; i++)
             {
-                var part = int.Parse(parts[i]);
-                var previous = int.Parse(parts[i - 1]);
+                var part = parts[i];
+                var previous = parts[i - 1];
                 var b = part > previous;
                 
                 //setting the initial value
@@ -59,6 +60,41 @@
                 if ((b != increasing) || diff is < 1 or > 3)
                 {
                     return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool IsSafeWithDampener(List<int> parts)
+        {
+            var increasing = false;
+            
+            //counting
+            for (var i = 0; i < parts.Count - 1; i++)
+            {
+                var part = parts[i];
+                var next = parts[i + 1];
+                var b = next > part;
+                
+                //setting the initial value
+                if (i == 0)
+                    increasing = b;
+                
+                var diff = Math.Abs(part - next);
+
+                if ((b != increasing) ||(diff is < 1 or > 3))
+                {
+                    bool safe = false;
+                    
+                    var removedI = parts.Where((_, idx) => idx != i).ToList();
+                    if (IsSafe(removedI)) return true;
+
+                    // Try removing i+1
+                    var removedNext = parts.Where((_, idx) => idx != i + 1).ToList();
+                    if (IsSafe(removedNext)) return true;
+                    
+                    Console.WriteLine(safe);
+                    return safe;
                 }
             }
             return true;
