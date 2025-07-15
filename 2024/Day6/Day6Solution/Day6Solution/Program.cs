25840938dc802b7e4b33b5834c6d93c7073da84f
Joris <wassinkjoris@gmail.com>
2025-07-15 14:02:45 +0200

    Add Day 6 Part 2

--- 2024/Day6/Day6Solution/Day6Solution/Program.cs
+++ 2024/Day6/Day6Solution/Day6Solution/Program.cs
@@ -28,9 +28,9 @@ namespace Day6Solution
 
     internal class Solution
     {
+        public static List<List<FloorTypes>> map = new List<List<FloorTypes>>();
         public static void solution(string path)
         {
-            var map = new List<List<FloorTypes>>();
             var currentPosition = Vector2.Zero;
             var sr = new StreamReader(path);
             var lineCount = 0;
@@ -66,7 +66,8 @@ namespace Day6Solution
                 map.Add(lineList);
             }
             
-            var amountToWalk = SimulateGuard(map, currentPosition);
+            var startDirection = new Vector2(-1, 0);
+            var amountToWalk = GetAllLoopOptions(currentPosition, startDirection);
             
             Console.WriteLine("Total amount: " + amountToWalk);
         }
@@ -106,6 +107,89 @@ namespace Day6Solution
                 }
             }
             return amountWalked;
+        }
+
+        private static int GetAllLoopOptions(Vector2 startPosition, Vector2 startDirection)
+        {
+            var stack = new Stack<(Vector2 position, Vector2 direction)>();
+            var visited = new HashSet<(Vector2, Vector2)>();
+            var totalAmount = 0;
+            stack.Push((startPosition, startDirection));
+            while (stack.Count > 0)
+            {
+                var (currentPosition, currentDirection) = stack.Pop();
+
+                if (visited.Contains((currentPosition, currentDirection)))
+                {
+                    continue;
+                }
+
+                // 5. Mark the current state as visited.
+                visited.Add((currentPosition, currentDirection));
+                
+                var posToCheck = currentPosition + currentDirection;
+                if (IsOutOfBounds(posToCheck))
+                    break;
+                
+                //check position
+                var floorTypeAtPos = map[(int)posToCheck.X][(int)posToCheck.Y];
+                
+                switch (floorTypeAtPos)
+                {
+                    case FloorTypes.HaveNotWalked:
+                        var passed = new HashSet<(Vector2, Vector2)>();
+                        var isLoop = IsLoopDFS(startPosition, new Vector2(-1,0),posToCheck, passed);
+                        if (isLoop) totalAmount += 1;
+                        map[(int)posToCheck.X][(int)posToCheck.Y] = FloorTypes.HaveWalked;
+                        stack.Push((posToCheck, currentDirection));
+                        break;
+                    case FloorTypes.HaveWalked:
+                        stack.Push((posToCheck, currentDirection));
+                        break;
+                    case FloorTypes.Obstacle:
+                        //rotate the vector2 90 degrees
+                        currentDirection = new Vector2(currentDirection.Y, -currentDirection.X);
+                        stack.Push((currentPosition, currentDirection));
+                        break;
+                    default:
+                        Console.WriteLine($"already been here!");
+                        break;
+                }
+                
+            }
+            return totalAmount;
+        }
+        
+        
+        private static bool IsLoopDFS(Vector2 position, Vector2 direction, Vector2 obstacle, HashSet<(Vector2, Vector2)> visited)
+        {
+            var isVisited = visited.Contains((position, direction));
+            if (isVisited)
+                return true; // cycle found
+
+            visited.Add((position, direction));
+            
+            var next = position + direction;
+            
+            if (IsOutOfBounds(next))
+                return false;
+
+            if (!(IsObstacle(next) || next == (obstacle)))
+                return IsLoopDFS(next, direction, obstacle, visited);
+            
+            var newDirection = new Vector2(direction.Y, -direction.X);
+            return IsLoopDFS(position, newDirection, obstacle, visited);
+            
+        }
+
+        private static bool IsObstacle(Vector2 position)
+        {
+            return map[(int)position.X][(int)position.Y] == FloorTypes.Obstacle;
+        }
+
+        private static bool IsOutOfBounds(Vector2 position)
+        {
+            return position.X < 0 || position.X > map.Count - 1 || position.Y < 0 || position.Y >= map[0].Count;
         }
     }
 }
